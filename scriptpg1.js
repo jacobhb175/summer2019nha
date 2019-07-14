0ce9f6ab1991ed49a83c24cf6dfaba8632247814
Jacob Hardin-Bernhardt <jAKEHB2@gmail.com>
2019-07-14 16:50:30 -0700

    Easier way to select jobs and lands

--- scriptpg1.js
+++ scriptpg1.js
@@ -1,48 +1,14 @@
 $(document).ready(function(){
  
 	//Set job
-	/*$("#farmer").click(function(){
-    	const job = "farmer";
-	});
-	$("#artisan").click(function(){
-		const job = "artisan";
-    });
-	$("#knight").click(function(){
-    	const job = "knight";
-    });*/
-    $("button").click(function(){
-    	alert(this.id); // or alert($(this).attr('id'));
-    	//if ("#id" == "farmer") {}(){
-    	//	console.log("farmer")
-    	//}
+    $(".job").click(function(){
+    	const job = this.id;
+    	console.log("job "+job)
     });
 
     //Set lands
-    $("#crown").click(function(){
-    	const lands = "crown";
-	});
-	$("#river").click(function(){
-    	const lands = "river";
-	});
-	$("#storm").click(function(){
-    	const lands = "storm";
-	});
-	$("#dorne").click(function(){
-    	const lands = "dorne";
-	});
-	$("#reach").click(function(){
-    	const lands = "reach";
-	});
-	$("#west").click(function(){
-    	const lands = "west";
-	});
-	$("#iron").click(function(){
-    	const lands = "iron";
-	});
-	$("#north").click(function(){
-    	const lands = "north";
-	});
-	$("#vale").click(function(){
-    	const lands = "vale";
-	});
+    $(".lands").click(function(){
+    	const lands = this.id;
+   		console.log("lands "+lands)
+    });
 });
